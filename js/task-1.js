cf5c41acda921b37ba490d9587198817c0eeb23a
MarinaV27 <Vasilishinamari@gmail.com>
2024-02-26 23:42:52 +0200

    task 4

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,10 @@
-const categories = document.querySelectorAll("ul#categories > li");
-console.log(`Number of categories: ${categories.length}`);
+const categories = document.querySelectorAll("ul#categories > li")
+      console.log(`Number of categories: ${categories.length}`);
 const titles = document.querySelectorAll("h2"); 
 titles.forEach (function (title) {
-    const  titleName = title.textContent;
+const  titleName = title.textContent;
       console.log(`Category: ${titleName}`);
-      const categoriesNames = title.parentElement.querySelectorAll("ul > li");
+const categoriesNames = title.parentElement.querySelectorAll("ul > li");
       console.log(`Elements: ${categoriesNames.length}`);
 });
 
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", handleSubmit);
+function handleSubmit(event){
+    event.preventDefault();
+const elements = event.currentTarget.elements;
+const info = { 
+    email: elements.email.value,
+    password: elements.password.value,
+}  
+if (info.email === "" || info.password === "") {
+    alert ("All form fields must be filled in");
+  }
+event.currentTarget.reset();
+}
+
+
+
+
+
+
 
