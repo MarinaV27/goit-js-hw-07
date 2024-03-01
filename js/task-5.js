aac89826c0bb1e65df31caf184a6dae9d5f315f6
MarinaV27 <Vasilishinamari@gmail.com>
2024-03-01 22:17:08 +0200

    task 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,23 @@
+
+const button = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+button.addEventListener ("click", () => {
+  let hexColor = getRandomHexColor()
+  document.body.style.backgroundColor = hexColor;
+  color.textContent = hexColor;
+
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+
+
+
+
+
+
